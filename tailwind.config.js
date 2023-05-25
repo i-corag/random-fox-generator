8f68a696919424838d8285fea50c37d8f0cf9442
i-corag <icoraglio@gmail.com>
2023-05-26 01:13:31 +0200

    add google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Quicksand', 'system-ui'],
+      },
+    },
   },
   plugins: [],
 };
